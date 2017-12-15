073cfc3a5ae73247325691b98a6aa70593e02887
John Carlo Lopez <condottiere.jc@gmail.com>
2017-12-15 14:33:27 -0500

    complete exercise 3

--- Player.rb
+++ Player.rb
@@ -1,6 +1,48 @@
 #1
 class Player
+  #init
   def initialize()
+    #2,3,4,5
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+  end
 
+  #instance methods
+  #6
+  def level_up
+    @lives += 1
+  end
+  #7
+  def collect_treasure
+    @gold_coins += 1
+    if ((@gold_coins % 10) == 0) && (@gold_coins != 0)
+      level_up
+
+    end
+  end
+  #8
+  def do_battle(damage)
+    @health_points -= damage
+    if (@health_points <= 0) && (@lives == 0)
+      restart
+    elsif @health_points <= 0
+      @health_points = 10
+      @lives -= 1
+    end
+  end
+  #9
+  def restart
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+  end
+
+  #testing
+  def about
+    "Your stats:\n
+    Gold:#{@gold_coins}\n
+    Health:#{@health_points}\n
+    Lives:#{@lives}"
   end
 end
--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,16 @@
 require "./Player.rb"
+
+john = Player.new
+
+puts john.about
+#6,7 test
+11.times {john.collect_treasure}
+puts john.about
+
+#8,9 test
+john.do_battle(10)
+puts john.about
+john.do_battle(9)
+puts john.about
+6.times {john.do_battle(10)}
+puts john.about
